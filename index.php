e8f5494c62b497ff9c06045910f5dfbee3c28754
Mark Rueckschnat <Mark@Marks-iMac.local>
2017-02-17 00:55:18 -0400

    Added module two

--- index.php
+++ index.php
@@ -4,12 +4,22 @@
 	<title></title>
 </head>
 <body>
+	<module-one>
+		<div>
+			<ul>
+				<li>Testing</li>
+				<li>Testing 2</li>
+			</ul>
+		</div>
+	</module-one>
 
-	<div>
-		<ul>
-			<li>Testing</li>
-			<li>Testing 2</li>
-		</ul>
-	</div>
+	<module-two>
+		
+		<div>
+			<ul>
+				<li>Item 1</li>
+			</ul>
+		</div>
+	</module-two>
 </body>
 </html>
